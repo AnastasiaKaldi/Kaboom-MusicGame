d1b868cd7571ab94be0bd869db1faf6a4a504e5e
AnastasiaKaldi <kaldianastasia@gmail.com>
2023-12-29 21:33:30 +0000

    Under Construction

--- music/www/game.js
+++ music/www/game.js
@@ -27,40 +27,41 @@ loadSprite("pipe-top-left", "./sprites/pipe-top-left.png");
 loadSprite("pipe-top-right", "./sprites/pipe-top-right.png");
 loadSprite("pipe-bottom-left", "./sprites/pipe-bottom-left.png");
 loadSprite("pipe-bottom-right", "./sprites/pipe-bottom-right.png");
-loadSprite("Knight", "./sprites/Idle.png", {
+
+loadSprite("Knight", "./sprites/knight/Idle.png", {
   sliceX: 4,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 3, loop: true } },
+  anims: { "idle-anim": { from: 0, to: 3, loop: true, speed: 1 } },
 });
-loadSprite("Attack", "./sprites/Attack1.png", {
+loadSprite("Attack", "./sprites/knight/Attack1.png", {
   sliceX: 6,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 5, loop: true } },
+  anims: { "attack-anim": { from: 0, to: 5, loop: true, speed: 1 } },
 });
-loadSprite("Fall", "./sprites/Fall.png", {
+loadSprite("Fall", "./sprites/knight/Fall.png", {
   sliceX: 3,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 2, loop: true } },
+  anims: { "fall-anim": { from: 0, to: 2, loop: true, speed: 1 } },
 });
-loadSprite("Jump", "./sprites/Jump.gif", {
+loadSprite("Jump", "./sprites/knight/Jump.gif", {
   sliceX: 6,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 5, loop: true } },
+  anims: { "jump-anim": { from: 0, to: 5, loop: true, speed: 1 } },
 });
-loadSprite("Run", "./sprites/Dashing.png", {
+loadSprite("Dashing", "./sprites/knight/Dashing.gif", {
   sliceX: 4,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 3, loop: true } },
+  anims: { "run-anim": { from: 0, to: 3, loop: true, speed: 1 } },
 });
-loadSprite("Walk", "./sprites/Walking.png", {
+loadSprite("Walk", "./sprites/knight/Walking.png", {
   sliceX: 7,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 6, loop: true } },
+  anims: { "walk-anim": { from: 0, to: 6, loop: true, speed: 1 } },
 });
-loadSprite("Landing", "./sprites/Landing.png", {
+loadSprite("Landing", "./sprites/knight/Landing.png", {
   sliceX: 4,
   sliceY: 1,
-  anims: { "idle-anim": { from: 0, to: 3, loop: true } },
+  anims: { "land-anim": { from: 0, to: 3, loop: true, speed: 1 } },
 });
 
 loadSprite("blue-block", "./sprites/blueblock.png");
@@ -68,8 +69,6 @@ loadSprite("blue-brick", "./sprites/bluebrick.png");
 loadSprite("blue-steel", "./sprites/bluesteel.png");
 loadSprite("blue-evil-shroom", "./sprites/bluemushroom.png");
 loadSprite("blue-surprise", "./sprites/bluesurprise.png");
-
-gravity(1000);
 
 scene("game", ({ level, score }) => {
   layers(["bg", "obj", "ui"], "obj");
@@ -179,72 +178,6 @@ scene("game", ({ level, score }) => {
     };
   }
 
-  const playerAnimations = {
-    idle: animation(0.2, ""),
-    jump: animation(
-      0.2,
-      "jump-0",
-      "jump-1",
-      "jump-2",
-      "jump-3",
-      "jump-4",
-      "jump-5",
-      "jump-6",
-      "jump-7",
-      "jump-8",
-      "jump-9",
-      "jump-10",
-      "jump-11",
-      "jump-12",
-      "jump-13",
-      "jump-14",
-      "jump-15"
-    ),
-    run: animation(
-      0.1,
-      "run-0",
-      "run-1",
-      "run-2",
-      "run-3",
-      "run-4",
-      "run-5",
-      "run-6",
-      "run-7",
-      "run-8",
-      "run-9",
-      "run-10",
-      "run-11",
-      "run-12",
-      "run-13",
-      "run-14",
-      "run-15"
-    ),
-    walk: animation(
-      0.1,
-      "walk-0",
-      "walk-1",
-      "walk-2",
-      "walk-3",
-      "walk-4",
-      "walk-5",
-      "walk-6",
-      "walk-7",
-      "walk-8",
-      "walk-9",
-      "walk-10",
-      "walk-11",
-      "walk-12",
-      "walk-13",
-      "walk-14",
-      "walk-15",
-      "walk-16",
-      "walk-17",
-      "walk-18",
-      "walk-19",
-      "walk-20"
-    ),
-  };
-
   const player = add([
     sprite("Knight"),
     solid(),
@@ -252,8 +185,7 @@ scene("game", ({ level, score }) => {
     body(),
     big(),
     origin("bot"),
-    scale(0.1),
-    area({ shape: new Rect(vec2(0), 32, 32), offset: vec2(0, 32) }),
+    scale(0.5),
   ]);
 
   player.play("idle-anim");
@@ -314,71 +246,92 @@ scene("game", ({ level, score }) => {
       });
     });
   });
+  player.action(() => {
+    if (player.grounded()) {
+      isJumping = false;
 
-  onKeyDown("right", () => {
-    if (player.curAnim() !== "Dashing" && player.isGrounded()) {
-      player.use(sprite("Dashing"));
-      player.play("run-anim");
+      if (
+        player.curAnim() !== "idle-anim" &&
+        !keyIsDown("left") &&
+        !keyIsDown("right")
+      ) {
+        player.use(sprite("Knight"));
+        player.play("idle-anim");
+      }
+
+      if (keyIsDown("left")) {
+        if (player.curAnim() !== "run-anim") {
+          player.use(sprite("Dashing"));
+          player.play("run-anim");
+        }
+        player.move(-MOVE_SPEED, 0);
+        player.flipX = true;
+      }
+
+      if (keyIsDown("right")) {
+        if (player.curAnim() !== "run-anim") {
+          player.use(sprite("Dashing"));
+          player.play("run-anim");
+        }
+        player.move(MOVE_SPEED, 0);
+        player.flipX = false;
+      }
+
+      if (keyIsPressed("up")) {
+        if (!isJumping) {
+          player.jump(CURRENT_JUMP_FORCE);
+          isJumping = true;
+        }
+      }
+
+      if (!keyIsDown("left") && !keyIsDown("right") && !keyIsDown("up")) {
+        if (player.curAnim() !== "idle-anim") {
+          player.use(sprite("Knight"));
+          player.play("idle-anim");
+        }
+      }
+    } else {
+      if (player.heightDelta > 0) {
+        if (player.curAnim() !== "jump-anim") {
+          player.use(sprite("Jump"));
+          player.play("jump-anim");
+        }
+      } else if (player.heightDelta < 0) {
+        if (player.curAnim() !== "fall-anim") {
+          player.use(sprite("Fall"));
+          player.play("fall-anim");
+        }
+      }
     }
-
-    if (player.direction !== "right") player.direction = "right";
-
-    player.move(player.speed, 0);
   });
 
-  onKeyRelease("right", () => {
-    player.use(sprite("Knight"));
-    player.play("idle-anim");
-  });
-  onKeyDown("left", () => {
-    if (player.curAnim() !== "Dashing" && player.isGrounded()) {
-      player.use(sprite("Dashing"));
-      player.play("run-anim");
-    }
-
-    if (player.direction !== "left") player.direction = "left";
-
-    player.move(-player.speed, 0);
-  });
-
-  onKeyRelease("left", () => {
-    player.use(sprite("Knight"));
-    player.play("idle-anim");
-  });
-
-  onKeyPress("up", () => {
-    if (player.isGrounded()) {
-      player.jump();
-    }
-  });
-
-  onUpdate(() => {
+  action(() => {
     if (player.previousHeight) {
       player.heightDelta = player.previousHeight - player.pos.y;
     }
 
     player.previousHeight = player.pos.y;
 
-    const cameraLeftBound = 550;
-    const cameraRightBound = 3000;
-    const cameraVerticalOffset = player.pos.y - 100;
+    // const cameraLeftBound = 550;
+    // const cameraRightBound = 3000;
+    // const cameraVerticalOffset = player.pos.y - 100;
 
-    if (cameraLeftBound > player.pos.x) {
-      camPos(cameraLeftBound, cameraVerticalOffset);
-    } else if (cameraRightBound < player.pos.x) {
-      camPos(cameraRightBound, cameraVerticalOffset);
-    } else {
-      camPos(player.pos.x, cameraVerticalOffset);
-    }
+    // if (cameraLeftBound > player.pos.x) {
+    //   camPos(cameraLeftBound, cameraVerticalOffset);
+    // } else if (cameraRightBound < player.pos.x) {
+    //   camPos(cameraRightBound, cameraVerticalOffset);
+    // } else {
+    //   camPos(player.pos.x, cameraVerticalOffset);
+    // }
 
-    if (player.curAnim() !== "run-anim" && player.isGrounded()) {
+    if (player.curAnim() !== "run-anim" && player.grounded()) {
       player.use(sprite("Knight"));
       player.play("idle-anim");
     }
-
+    //player.player.grounded()
     if (
       player.curAnim() !== "Jump" &&
-      !player.isGrounded() &&
+      !player.grounded() &&
       player.heightDelta > 0
     ) {
       player.use(sprite("Jump"));
@@ -387,7 +340,7 @@ scene("game", ({ level, score }) => {
 
     if (
       player.curAnim() !== "Fall" &&
-      !player.isGrounded() &&
+      !player.grounded() &&
       player.heightDelta < 0
     ) {
       player.use(sprite("Fall"));
@@ -401,14 +354,14 @@ scene("game", ({ level, score }) => {
     }
   });
 
-  player.action(() => {
-    if (player.grounded()) {
-      isJumping = false;
-      if (!keyIsDown("left") && !keyIsDown("right")) {
-        player.play(playerAnimations.idle); // Change animation to idle when player stops moving
-      }
-    }
-  });
+  // player.action(() => {
+  //   if (player.grounded()) {
+  //     isJumping = false;
+  //     if (!keyIsDown("left") && !keyIsDown("right")) {
+  //       player.play("idle-anim");
+  //     }
+  //   }
+  // });
 
   // ... The rest of the game logic
 });
@@ -427,7 +380,7 @@ scene("lose", ({ score }) => {
   ]);
 
   // Key event listener for spacebar to restart the game
-  keyPress("enter", () => {
+  keyPress("space", () => {
     go("game", { level: 0, score: 0 }); // Restart the game with initial level and score
   });
 });
